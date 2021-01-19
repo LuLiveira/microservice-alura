e2fa8c550be443900019c46834622dcc7dda7da6
Lucas Silva <lcsd.lucas@gmail.com>
2021-01-18 23:25:27 -0300

    finalizando

--- loja/src/main/java/br/com/alura/microservice/loja/LojaApplication.java
+++ loja/src/main/java/br/com/alura/microservice/loja/LojaApplication.java
@@ -5,13 +5,36 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 import org.springframework.web.client.RestTemplate;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableResourceServer
 public class LojaApplication {
+	
+	@Bean
+	public RequestInterceptor getRequestInterceptorDeAutenticacao() {
+		return new RequestInterceptor() {
+			@Override
+			public void apply(RequestTemplate template) {
+				Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+				if(authentication == null) {
+					return;
+				}
+				
+				OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails) authentication.getDetails();
+				String token = details.getTokenValue();
+				template.header("Authorization", "Bearer " + token);
+			}
+		};
+	}
 	
 	@Bean
 	@LoadBalanced
